247abfe795fcbbd91f7462d08fffbce5f76ba381
Ryan Xin <xinpf0715@gmail.com>
2022-04-27 23:38:56 +1000

    all code

---
+++ src/components/app.component.jsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+export class App extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <h1>React SSR</h1>
+    );
+  }
+}
